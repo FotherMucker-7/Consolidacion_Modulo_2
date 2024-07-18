ee5a5a0c4911e9be5d940fe00aad66893410dc57
FotherMucker-7 <pedromsilvar.20@gmail.com>
2024-07-18 11:35:29 -0400

    segundo commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,26 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Gracias por contactarte. Pronto te responderé.');
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.move-on-hover');
+
+    images.forEach(image => {
+        image.addEventListener('mouseover', function() {
+            image.style.transform = 'translateY(-10px)';
+        });
+
+        image.addEventListener('mouseout', function() {
+            image.style.transform = 'translateY(0)';
+        });
+    });
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevenir el comportamiento por defecto del formulario
+
+    // Aquí puedes añadir el código para procesar y enviar los datos del formulario
+    
+    // Mostrar un mensaje de confirmación
+    document.getElementById('confirmation').style.display = 'block';
+
+    // Borrar los datos del formulario
+    this.reset();
+});
+
